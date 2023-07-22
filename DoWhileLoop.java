cafab362f4f3bcf1aeeb9a8058e57dbf13a750d5
Adarsh <am9774981@gmail.com>
2023-07-22 19:59:10 +0530

    first commit

---
+++ DoWhileLoop.java
@@ -0,0 +1,10 @@
+public class DoWhileLoop {
+    public static void main(String arms[]){
+        int i = 0;
+        do{
+            System.out.println(i);
+            i++;
+        }while( i<10);
+
+    }
+}
